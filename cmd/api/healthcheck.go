4873106bbc2b63326105c8dd9fed09b92b299f9b
gopheramit <amitwpu@gmail.com>
2021-10-21 12:48:48 +0530

    sending json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,23 +5,15 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
-	// Set the "Content-Type: application/json" header on the response. If you forget to
-	// this, Go will default to sending a "Content-Type: text/plain; charset=utf-8"
-	// header instead.
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("ccess-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "*")
-
-	// Write the JSON as the HTTP response body.
-
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
